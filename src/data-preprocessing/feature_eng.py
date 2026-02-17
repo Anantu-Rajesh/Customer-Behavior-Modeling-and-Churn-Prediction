d3cebf1d908108d6de77fafdd1d8860624789474
Anantu-Rajesh <rajeshanantu@gmail.com>
2026-02-18 00:22:45 +0530

    fourth commit-cancellation features, derived features and customer df created, labels left

--- src/data-preprocessing/feature_eng.py
+++ src/data-preprocessing/feature_eng.py
@@ -23,10 +23,11 @@ To-Dos:
    - Max_Order_Value (largest single purchase)
    - Std_Order_Value (order amount variability)
 
-5. CANCELLATION FEATURES (Customer-level):
+5. CANCELLATION FEATURES (Customer-level): DONE BBG
    - Total_Cancelled (sum of cancelled amounts)
    - Num_Cancellations (count of cancelled invoices)
    - Total_Items_Cancelled (sum of cancelled quantities)
+   - Days_Since_Last_Cancellation (reference_date - max cancellation date)
 
 6. DERIVED FEATURES:
    - Net_Revenue (Total_Purchased - could also track if you want gross)
@@ -67,17 +68,24 @@ To-Dos:
 
 #1,2: finding reference date & dividing dataset wrt reference date
 def set_reference_date(df,reference_date=None):
+    
+    print("Total unique customers in cleaned data:", df['customerid'].nunique())
+
     if reference_date is None:
         reference_date = df['invoicedate'].max() - pd.DateOffset(months=3)
     print(f"Reference date set to: {reference_date.date()}")
     
     df_before=df[df['invoicedate']<=reference_date].copy()
+    # Before reference date
+    print("Customers with transactions before reference date:", df_before['customerid'].nunique())
     print(f"Transactions before reference date ({reference_date.date()}): {df_before.shape[0]}")
 
     df_after=df[df['invoicedate']>reference_date].copy()
+    # After reference date
+    print("Customers with transactions after reference date:", df_after['customerid'].nunique())
     print(f"Transactions after reference date ({reference_date.date()}): {df_after.shape[0]}")
+    
     return df_before,df_after,reference_date
-
 
 #4,7: handling purchase features and recency features(wrt reference date)
 def purchase_features(df_before,reference_date):
@@ -125,15 +133,59 @@ def purchase_features(df_before,reference_date):
     df_purchase['min_order_val'] = df_purchase['min_order_val'].fillna(df_purchase['max_order_val'])
     
     return df_purchase
+
+#5: handling cancellation features(wrt reference date)
+def cancellation_features(df_before,reference_date):
+    df_temp2=df_before[df_before['is_cancellation']].copy()
+    print("Customers with cancellations b4 reference date:", df_temp2['customerid'].nunique())
     
+    df_cancel=df_temp2.groupby('customerid').agg(
+        total_cancellation_count=('is_cancellation','nunique'),
+        total_cancellation_amnt=('cancel_amnt','sum'),
+        total_cancelled_qty=('cancel_qty','sum'),
+        last_cancel_date=('invoicedate','max')
+        ).reset_index()
+    
+    df_cancel['days_since_last_cancellation']=(reference_date-df_cancel['last_cancel_date']).dt.days
+    
+    
+    return df_cancel
+
+def merge_datasets(df_purchase,df_cancel):
+    customer_df=df_purchase.merge(df_cancel,on='customerid',how='left')
+    
+    customer_df['total_cancellation_count']=customer_df['total_cancellation_count'].fillna(0)
+    customer_df['total_cancellation_amnt']=customer_df['total_cancellation_amnt'].fillna(0)
+    customer_df['total_cancelled_qty']=customer_df['total_cancelled_qty'].fillna(0)
+    customer_df['days_since_last_cancellation']=customer_df['days_since_last_cancellation'].fillna(1000)
+    
+    return customer_df
+
+def derive_features(customer_df):
+    customer_df['cancellation_rate'] = (customer_df['total_cancellation_count'] / (customer_df['count_orders'] + customer_df['total_cancellation_count'])).fillna(0)
+    customer_df['order_completion_rate'] = customer_df['count_orders'] / (customer_df['count_orders'] + customer_df['total_cancellation_count'])
+    customer_df['return_purchase_ratio'] = customer_df['total_cancelled_qty'] / customer_df['tot_items'].fillna(0).replace([np.inf], 0)
+    
+    customer_df = customer_df.replace([np.inf, -np.inf], 0)
+    
+    return customer_df
 
 def feature_eng(df):
     df_before,df_after,reference_date=set_reference_date(df,reference_date=None)
     df_purchase=purchase_features(df_before,reference_date)
+    df_cancel=cancellation_features(df_before,reference_date)
+    customer_df=merge_datasets(df_purchase,df_cancel)
+    customer_df=derive_features(customer_df)
     print(f"df_purchase shape: {df_purchase.shape}")
     print(f"purchase features:\n")
     ld.dataset_overview(df_purchase)
-    
+    print(f"df_cancel shape: {df_cancel.shape}")
+    print(f"cancel features:\n")
+    ld.dataset_overview(df_cancel)
+    print(f"\ncustomer_df shape: {customer_df.shape}")
+    print(f"\ncustomer_df col list:\n {list(customer_df.columns)}")
+    print(f"\ncustomer_df:\n")   
+    ld.dataset_overview(customer_df)
 
 if __name__ == "__main__":
     df = ld.load_and_describe_data(config.org_filepath)
